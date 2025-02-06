08ae7b650f29c3da109e4673f15c1672fe31a95d
Yakov Shepelev <yasha.shepeleff@gmail.com>
2025-02-06 19:51:46 +0500

    Задание 3.3

---
+++ main.py
@@ -0,0 +1,21 @@
+import classGame
+import pygame
+
+game = classGame.Game()
+
+pygame.time.set_timer(pygame.USEREVENT, 400)
+TIMESPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(TIMESPEED, 7000)
+
+
+while not game.over:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            game.over = True
+        if e.type == pygame.USEREVENT:
+            game.spawnbaloon()
+        if e.type == pygame.MOUSEBUTTONDOWN:
+            game.checkclick(e.pos)
+        if e.type == TIMESPEED:
+            game.speedup()
+    game.update()
